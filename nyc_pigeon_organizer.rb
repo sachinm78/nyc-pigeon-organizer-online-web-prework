6122f75ac2b170e50ee581c670bae63146638e01
Sachin Mahajan <sachinm78@gmail.com>
2019-03-06 17:05:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,13 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  
+  data.each do |trait, attributes|
+    attributes.each do |details, names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][trait] ||= []
+        pigeon_list[name][trait] << details.to_s
+      end
+    end
+  end
   return pigeon_list
 end
